b8104eb99f71de81bc5dbc2a6e5a15b27bf3a262
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-04-19 11:48:37 +0100

    Prevent multiple instances of prisma client

--- src/db.js
+++ src/db.js
@@ -1,10 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient({
-	log: [
-		'query'
-	]
-});
+// export const prisma = new PrismaClient({
+// 	log: [
+// 		'query'
+// 	]
+// });
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient({ log: ['query'] });
+	}
+	prisma = global.prisma;
+}
+
+export default prisma;
 
 	
 // import mysql from 'mysql'
