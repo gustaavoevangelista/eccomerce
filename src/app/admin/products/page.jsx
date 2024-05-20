67207f93bab2954afaa13f1e4b873c2abd783d3e
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-05-20 16:45:56 +0100

    Products table retrieving data from db

--- src/app/admin/products/page.jsx
+++ src/app/admin/products/page.jsx
@@ -1,55 +1,27 @@
+import prisma from "@/db";
 import styles from "./page.module.css"
 import { DataGrid } from '@mui/x-data-grid';
 
-const rows = [
-	{
-		id: 1,
-		col1: 'Teste',
-		col2: true,
-		col3: 'Hello',
-		col4: 'World',
-		col5: 19,
-		col6: 'Random Category',
-	},
-	{
-		id: 2,
-		col1: 'Teste',
-		col2: false,
-		col3: 'Hello',
-		col4: 'World',
-		col5: 19,
-		col6: 'Random Category',
-	},
-	{
-		id: 3,
-		col1: 'Teste',
-		col2: true,
-		col3: 'Hello',
-		col4: 'World',
-		col5: 19,
-		col6: 'Random Category',
-	},
-	{
-		id: 4,
-		col1: 'Teste',
-		col2: true,
-		col3: 'Hello',
-		col4: 'World',
-		col5: 19,
-		col6: 'Random Category',
-	}
-];
+const products = await prisma.product.findMany();
+
+const rows = products.map((product) => ({
+	id: product.id,
+	image: 'devolver imagem',
+	name: product.name,
+	isAvailable: product.isAvailable,
+	price: product.priceInCents,
+	category: product.categoryId,
+}));
 
 const columns = [
-	{ field: 'col1', headerName: 'Image', width: 100 },
-	{ field: 'col2', headerName: 'Name', width: 150 },
-	{ field: 'col3', headerName: 'Active', width: 100 },
-	{ field: 'col4', headerName: 'Price', width: 150 },
-	{ field: 'col5', headerName: 'Stock', width: 100 },
-	{ field: 'col6', headerName: 'Category', width: 150 },
+	{ field: 'image', headerName: 'Image', width: 100 },
+	{ field: 'name', headerName: 'Name', width: 150 },
+	{ field: 'isAvailable', headerName: 'Active', width: 100 },
+	{ field: 'price', headerName: 'Price', width: 150 },
+	{ field: 'category', headerName: 'Category', width: 150 },
 ];
 
-export default function AdminProductsPage() {
+export default async function AdminProductsPage() {
 	return (
 		<div className={styles.adminProductPage}>
 			<div className={styles.adminProductPageHeader}>
